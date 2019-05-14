c394eab69fd943395de2f2dcae6c4a180c3febd2
yangbin1 <yangbin199191@163.com>
2019-05-14 21:54:30 +0800

    444

--- src/main/java/com/yangbin1/product_service/controller/ProductController.java
+++ src/main/java/com/yangbin1/product_service/controller/ProductController.java
@@ -1,7 +1,10 @@
 package com.yangbin1.product_service.controller;
 
+import com.yangbin1.product_service.domain.Product;
 import com.yangbin1.product_service.service.ProductService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/product")
 public class ProductController {
+
+    @Value("${server.port}")
+    private String port;
 
     @Autowired
     private ProductService productService;
@@ -20,6 +26,10 @@ public class ProductController {
 
     @RequestMapping("find")
     public Object findById(@RequestParam("id") int id) {
-        return productService.findById(id);
+        Product product = productService.findById(id);
+        Product result = new Product();
+        BeanUtils.copyProperties(product,result);
+        result.setName(result.getName() + "data from port=" + port);
+        return result;
     }
 }
